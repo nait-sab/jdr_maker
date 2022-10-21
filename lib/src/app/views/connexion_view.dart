86bb0c357824954be11d71697243a7039f0fa9d7
Romain <73525560+Rom10811@users.noreply.github.com>
2022-10-21 14:22:09 +0200

    Ajout connexion view, inscription button accueil

--- lib/src/app/views/connexion_view.dart
+++ lib/src/app/views/connexion_view.dart
@@ -18,9 +18,132 @@ class _ConnexionViewState extends State<ConnexionView> {
 
   @override
   Widget build(BuildContext context) {
+    Size largeurEcran = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(),
+      backgroundColor: Color(0xFF19191D),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Center(
+          //   child: SizedBox(
+          //     width: largeurEcran.width / 2.4,
+          //     child: Column(
+          //       crossAxisAlignment: CrossAxisAlignment.start,
+          //       children: [
+          //         Padding(
+          //           padding: EdgeInsets.only(left: 10),
+          //           child: Text(
+          //             "Nom d'utilisateur",
+          //             style: TextStyle(fontSize: 20, color: Colors.white),
+          //           ),
+          //         ),
+          //         SizedBox(height: 15),
+          //         TextFormField(
+          //           controller: userNameController,
+          //           decoration: InputDecoration(
+          //             fillColor: Colors.white,
+          //             filled: true,
+          //             hintText: "Nom d'utilisateur",
+          //             border: OutlineInputBorder(
+          //               borderRadius: BorderRadius.circular(30),
+          //             ),
+          //             focusedBorder: OutlineInputBorder(
+          //               borderRadius: BorderRadius.all(Radius.circular(30)),
+          //               borderSide: BorderSide(color: Colors.white),
+          //             ),
+          //           ),
+          //         ),
+          //       ],
+          //     ),
+          //   ),
+          // ),
+          // SizedBox(height: 20),
+          Center(
+            child: SizedBox(
+              width: largeurEcran.width / 2.4,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Adresse Mail",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: "Adresse Mail",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: SizedBox(
+              width: largeurEcran.width / 2.4,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Mot de passe",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: "Mot de passe",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 50),
+          InkWell(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xff36AC50),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: Text(
+                "Connexion",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
