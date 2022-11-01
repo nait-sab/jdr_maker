922b4291b0e3cd7f1b32250db1c3d06a7b15cf83
= <naitsab.boulogne@gmail.com>
2022-11-01 16:35:38 +0100

    Ajout d'un bouton déconnexion côté mobile dans les options

--- lib/src/app/views/accueil/pages/accueil_options.dart
+++ lib/src/app/views/accueil/pages/accueil_options.dart
@@ -1,16 +1,28 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:jdr_maker/src/app/controllers/navigation_controller.dart';
+import 'package:jdr_maker/src/app/widgets/bouton.dart';
+import 'package:jdr_maker/src/domain/data/couleurs.dart';
 
 /// Classe : Accueil - Options
 ///
 /// Type : Page
 ///
 /// Contient la page d'options de l'accueil
-class AccueilPageOptions extends StatelessWidget {
+class AccueilPageOptions extends StatefulWidget {
+  @override
+  State<AccueilPageOptions> createState() => _AccueilPageOptionsState();
+}
+
+class _AccueilPageOptionsState extends State<AccueilPageOptions> {
   @override
   Widget build(BuildContext context) {
     return Platform.isAndroid ? renduMobile(context) : renduDesktop(context);
+  }
+
+  void boutonDeconnexion() {
+    NavigationController.changerView(context, "/accueil");
   }
 
   Widget renduDesktop(BuildContext context) {
@@ -25,11 +37,32 @@ class AccueilPageOptions extends StatelessWidget {
   }
 
   Widget renduMobile(BuildContext context) {
-    return Center(
-      child: Text(
-        "Options",
-        style: TextStyle(
-          color: Colors.white,
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            optionBouton("Déconnexion", action: boutonDeconnexion),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget optionBouton(String texte, {VoidCallback? action}) {
+    return Bouton(
+      onTap: action ?? (() {}),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Couleurs.fondSecondaire,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.only(bottom: 20),
+        child: Text(
+          texte,
+          style: TextStyle(fontSize: 20, color: Colors.white),
         ),
       ),
     );
