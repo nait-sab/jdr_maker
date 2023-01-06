aa8de72f7df4e32edfb05ca6b8a39a02477bdd3d
= <naitsab.boulogne@gmail.com>
2023-01-06 18:15:19 +0100

    patch

--- lib/src/app/tools/get_random_nb.dart
+++ lib/src/app/tools/get_random_nb.dart
@@ -2,4 +2,6 @@ import 'dart:math';
 final _random = Random();
 
 /// Genere un nb aléatoire
+/// 
+/// [Deprecated] - Utiliser GenerateurTool.genererNombre(int valeurMax)
 int next(int max) => 1 + _random.nextInt((max+1) - 1);
